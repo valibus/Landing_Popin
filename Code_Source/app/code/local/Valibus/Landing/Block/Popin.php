602b3cabc152f2c9ecdd747e914e2cc3e1a66c82
valibus <pvalibus@gmail.com>
2014-08-13 13:14:55 +0200

    back office setup
    Listing cms block OK

---
+++ Code_Source/app/code/local/Valibus/Landing/Block/Popin.php
@@ -0,0 +1,61 @@
+<?php
+class Valibus_Landing_Block_Popin extends Mage_Core_Block_Template
+{
+    const COOKIE_NAME = 'landing';
+
+
+    //test if we show the popin (and the HTML & JS source code also)
+    public function canShow()
+    {
+
+        //on vérifie si le cookie existe
+        if ($this->readCookie()) {
+            $cookieLanding = $this->readCookie();
+            $cookieSetOn = $cookieLanding[0];
+            $cookieDisplay = $cookieLanding[1];
+            //on test si il n'est expiré
+            if (Mage::helper('landing')->getShowAgain($cookieSetOn)) {
+                $this->startCookie();
+                return true;
+            } elseif (Mage::helper('landing')->getShowFirst($cookieSetOn, $cookieDisplay)) {
+                $this->updateCookie();
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            $this->startCookie();
+            return false;
+        }
+    }
+
+    //Set cookie
+
+    public function startCookie()
+    {
+        $cookie = Mage::getModel('core/cookie');
+        $value = time() . '.0';
+        $cookie->set(self::COOKIE_NAME, $value, Mage::helper('landing')->getRepeatDelay());
+        return $cookie;
+    }
+
+    public function updateCookie()
+    {
+        $infos = $this->readCookie();
+        $cookie = Mage::getModel('core/cookie');
+        $value = $infos[0] . '.1';
+        $cookie->set(self::COOKIE_NAME, $value, Mage::helper('landing')->getRepeatDelay());
+        return $cookie;
+    }
+
+    public function readCookie()
+    {
+        $landingCookie = Mage::getModel('core/cookie');
+        if ($landingCookie->get(self::COOKIE_NAME)) {
+            $landingInfos = explode('.', $landingCookie->get(self::COOKIE_NAME));
+        } else {
+            return false;
+        }
+        return $landingInfos;
+    }
+}
---
+++ Code_Source/app/code/local/Valibus/Landing/Helper/Data.php
@@ -0,0 +1,4 @@
+<?php
+class Valibus_Landing_Helper_Data extends Mage_Core_Helper_Abstract{
+
+}
